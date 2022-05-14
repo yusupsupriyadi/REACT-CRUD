d639eb459f53b06efc121dfc45e3ef05e57d8eee
yusupsupriyadi <yusupsupriyadistudio@gmail.com>
2022-05-15 00:22:28 +0700

    started template

--- src/App.js
+++ src/App.js
@@ -1,145 +1,30 @@
 import "./App.css";
 import List from "./List";
-import { useState, useEffect } from "react";
-import { uid } from "uid";
-import axios from "axios";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => {
-    // Mengambil data API
-    axios.get("http://localhost:3000/contacts").then((response) => {
-      setContacts(response?.data ?? []);
-    });
-  }, []);
-
-  const [formData, setFormData] = useState({
-    name: "",
-    telp: "",
-  });
-
-  function handleChange(e) {
-    let data = { ...formData };
-    data[e.target.name] = e.target.value;
-    setFormData(data);
-  }
-
-  const [isUpdate, setIsUpdate] = useState({
-    id: null,
-    status: false,
-  });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    let data = [...contacts];
-
-    if (formData.name === "" || formData.telp === "") {
-      return false;
-    }
-
-    if (isUpdate.status === true) {
-      // Update Contact
-      data.forEach((contact) => {
-        if (contact.id === isUpdate.id) {
-          contact.name = formData.name;
-          contact.telp = formData.telp;
-        }
-      });
-
-      axios
-        .put(`http://localhost:3000/contacts/${isUpdate.id}`, {
-          name: formData.name,
-          telp: formData.telp,
-        })
-        .then((response) => {
-          alert("Data berhasil diupdate");
-        });
-    } else {
-      // Create contact
-      let newData = {
-        id: uid(),
-        name: formData.name,
-        telp: formData.telp,
-      };
-      data.push(newData);
-      axios.post("http://localhost:3000/contacts", newData).then((response) => {
-        alert("Data berhasil ditambahkan");
-      });
-    }
-
-    setContacts(data);
-    setFormData({
-      name: "",
-      telp: "",
-    });
-    setIsUpdate({
-      id: null,
-      status: false,
-    });
-  }
-
-  function handleEdit(id) {
-    let data = [...contacts];
-    let foundData = data.find((contact) => contact.id === id);
-
-    setFormData({
-      name: foundData.name,
-      telp: foundData.telp,
-    });
-
-    setIsUpdate({
-      id: id,
-      status: true,
-    });
-  }
-
-  function handleDelete(id) {
-    let data = [...contacts];
-    let filteredData = data.filter((contact) => contact.id !== id);
-
-    axios.delete(`http://localhost:3000/contacts/${id}`).then((response) => {
-      alert("Data berhasil dihapus");
-    });
-    setContacts(filteredData);
-  }
   return (
     <div className="App">
-      <h1 className="px-3 py-3">My Contact List</h1>
-
-      <form onSubmit={handleSubmit} className="px-3 py-4">
-        <div className="form-group">
-          <label htmlFor="">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={handleChange}
-            name="name"
-            value={formData.name}
-          />
-        </div>
-        <div className="form-group mt-3">
-          <label htmlFor="">No. Telp</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={handleChange}
-            name="telp"
-            value={formData.telp}
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary w-100 mt-3">
-            Save
-          </button>
-        </div>
-      </form>
-
-      <List
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-        data={contacts}
-      />
+      <div className="fixed-top bg-white pb-3 mx-auto" style={{ width: 400 }}>
+        <h1 className="px-3 py-3 font-weight-bold">My Contact List</h1>
+        <form className="px-3 py-4">
+          <div className="form-group">
+            <label htmlFor="">Name</label>
+            <input type="text" className="form-control" name="name" />
+          </div>
+          <div className="form-group mt-3">
+            <label htmlFor="">No. Telp</label>
+            <input type="text" className="form-control" name="telp" />
+          </div>
+          <div>
+            <button type="submit" className="btn btn-primary w-100 mt-3">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+      <div style={{ marginTop: 350 }}>
+        <List />
+      </div>
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,24 +1,28 @@
 import React from "react";
 
-export default function List({ data, handleEdit, handleDelete }) {
-  let key = 1;
+export default function List() {
   return (
     <div className="list-group">
-      {data.map((contact) => {
-        return (
-          <div className="list-group-item list-group-item-action" key={contact.id}>
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{key++}. {contact.name}
-              </h5>
-              <div>
-                <button onClick={() => handleEdit(contact.id)} className="btn btn-sm btn-link">Edit</button>
-                <button onClick={() => handleDelete(contact.id)} className="btn btn-sm btn-link">Del</button>
-              </div>
-            </div>
-            <p className="mb-1">Telp: {contact.telp}</p>
+      <div className="list-group-item list-group-item-action">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">John Doe</h5>
+          <div>
+            <button className="btn btn-sm btn-link">Edit</button>
+            <button className="btn btn-sm btn-link">Del</button>
           </div>
-        );
-      })}
+        </div>
+        <p className="mb-1">08815646413</p>
+      </div>
+      <div className="list-group-item list-group-item-action">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">Alex Pick</h5>
+          <div>
+            <button className="btn btn-sm btn-link">Edit</button>
+            <button className="btn btn-sm btn-link">Del</button>
+          </div>
+        </div>
+        <p className="mb-1">08815646413</p>
+      </div>
     </div>
   );
 }
